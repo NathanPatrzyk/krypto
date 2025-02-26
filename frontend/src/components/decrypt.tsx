1fa04b2aeec1889d38c0e1e44d16c1c80462cac7
Nathan Patrzyk <nathanpatzyk11@gmail.com>
2025-02-26 20:40:41 -0300

    v7.0

--- frontend/src/components/decrypt.tsx
+++ frontend/src/components/decrypt.tsx
@@ -10,8 +10,11 @@ import { Input } from "../components/ui/input";
 import { Label } from "../components/ui/label";
 import { KeyRound, LockKeyholeOpen } from "lucide-react";
 import LZString from "lz-string";
+import JSEncrypt from "jsencrypt";
 
 export default function Decrypt() {
+  const [loading, setLoading] = useState<boolean>(false);
+
   const [ciphertext, setCiphertext] = useState<string>("");
   const [threeDESCipherKey, setThreeDESCipherKey] = useState<string>("");
   const [RSAPrivateKey, setRSAPrivateKey] = useState<string>("");
@@ -32,33 +35,65 @@ export default function Decrypt() {
   function handleSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
 
-    alert(LZString.decompressFromEncodedURIComponent(ciphertext));
+    const decompressedText =
+      LZString.decompressFromEncodedURIComponent(ciphertext);
 
     setCiphertext("");
+    setThreeDESCipherKey("");
+    setRSAPublicKey("");
+    setRSAPrivateKey("");
   }
 
-  function handleGenerateRSAPrivateKey(event: MouseEvent<HTMLButtonElement>) {
+  function handleGenerateRSAKeys(event: MouseEvent<HTMLButtonElement>) {
     event.preventDefault();
 
-    const RSAPrivateKey = CryptoJS.lib.WordArray.random(24).toString(
-      CryptoJS.enc.HexRSAPrivateKey
-    );
-    setRSAPrivateKey(RSAPrivateKey);
-  }
-  function handleRSAPublicKey(event: ChangeEvent<HTMLInputElement>): void {
-    throw new Error("Function not implemented.");
-  }
+    setLoading(true);
 
-  function handleGenerateThreeDESKey(
-    event: MouseEvent<HTMLButtonElement, MouseEvent>
-  ): void {
-    throw new Error("Function not implemented.");
+    setTimeout(() => {
+      const crypt = new JSEncrypt({ default_key_size: "2048" });
+
+      const RSAPublicKey = crypt.getPublicKey();
+      const RSAPrivateKey = crypt.getPrivateKey();
+
+      setRSAPublicKey(RSAPublicKey);
+      setRSAPrivateKey(RSAPrivateKey);
+
+      setLoading(false);
+    }, 200);
   }
 
   return (
     <div className="w-full max-w-xl">
       <div className="flex flex-col gap-6">
         <Card>
+          {loading && (
+            <p className="w-full flex items-center justify-center gap-2 bg-primary text-primary-foreground rounded-t-xl p-2">
+              <svg
+                className="text-primary-foreground/30 animate-spin w-4 h-4"
+                viewBox="0 0 64 64"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M32 3C35.8083 3 39.5794 3.75011 43.0978 5.20749C46.6163 6.66488 49.8132 8.80101 52.5061 11.4939C55.199 14.1868 57.3351 17.3837 58.7925 20.9022C60.2499 24.4206 61 28.1917 61 32C61 35.8083 60.2499 39.5794 58.7925 43.0978C57.3351 46.6163 55.199 49.8132 52.5061 52.5061C49.8132 55.199 46.6163 57.3351 43.0978 58.7925C39.5794 60.2499 35.8083 61 32 61C28.1917 61 24.4206 60.2499 20.9022 58.7925C17.3837 57.3351 14.1868 55.199 11.4939 52.5061C8.801 49.8132 6.66487 46.6163 5.20749 43.0978C3.7501 39.5794 3 35.8083 3 32C3 28.1917 3.75011 24.4206 5.2075 20.9022C6.66489 17.3837 8.80101 14.1868 11.4939 11.4939C14.1868 8.80099 17.3838 6.66487 20.9022 5.20749C24.4206 3.7501 28.1917 3 32 3L32 3Z"
+                  stroke="currentColor"
+                  strokeWidth="5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                ></path>
+                <path
+                  d="M32 3C36.5778 3 41.0906 4.08374 45.1692 6.16256C49.2477 8.24138 52.7762 11.2562 55.466 14.9605C58.1558 18.6647 59.9304 22.9531 60.6448 27.4748C61.3591 31.9965 60.9928 36.6232 59.5759 40.9762"
+                  stroke="currentColor"
+                  strokeWidth="5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="text-primary-foreground"
+                ></path>
+              </svg>
+              Carregando...
+            </p>
+          )}
+
           <CardHeader>
             <CardTitle className="text-2xl">Descriptografar</CardTitle>
           </CardHeader>
@@ -100,38 +135,42 @@ export default function Decrypt() {
                     ></Input>
                     <Button
                       className="w-full sm:w-auto"
-                      onClick={handleGenerateRSAPrivateKey}
+                      onClick={handleGenerateRSAKeys}
                     >
                       <KeyRound />
                       Gerar
                     </Button>
                   </div>
                 </div>
-                {/* <div className="grid gap-2">
-                  <Label htmlFor="text">Chave Pública RSA (2048 bits)</Label>
-                  <Input
-                    id="RSAPublicKey"
-                    type="text"
-                    placeholder="Sua chave privada RSA gerada aparecerá aqui..."
-                    required
-                    onChange={handleRSAPublicKey}
-                    value={RSAPublicKey}
-                  ></Input>
-                </div> */}
+                {RSAPublicKey && (
+                  <div className="grid gap-2">
+                    <p className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+                      Chave Pública RSA (2048 bits)
+                    </p>
+                    <p className="text-sm opacity-70 font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+                      Compartilhe esta chave com quem precisar enviar mensagens
+                      seguras para você, será útil para criptografar os dados
+                      antes do envio.
+                    </p>
+                    <div className="flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm overflow-x-auto whitespace-nowrap scrollbar-hide">
+                      {RSAPublicKey}
+                    </div>
+                  </div>
+                )}
 
                 <Button>
                   <LockKeyholeOpen />
                   Descriptografar
                 </Button>
 
-                {/* <div className="grid gap-2">
+                <div className="grid gap-2">
                   <p className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
                     Dados a serem enviados
                   </p>
                   <div className="flex w-full rounded-md border border-input bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm">
                     ...
                   </div>
-                </div> */}
+                </div>
               </div>
             </form>
           </CardContent>
--- frontend/src/components/encrypt.tsx
+++ frontend/src/components/encrypt.tsx
@@ -12,8 +12,11 @@ import { LockKeyhole, KeyRound } from "lucide-react";
 
 import LZString from "lz-string";
 import CryptoJS from "crypto-js";
+import { JSEncrypt } from "jsencrypt";
 
 export default function Encrypt() {
+  const [loading, setLoading] = useState<boolean>(false);
+
   const [text, setText] = useState<string>("");
   const [threeDESKey, setThreeDESKey] = useState<string>("");
   const [RSAPublicKey, setRSAPublicKey] = useState<string>("");
@@ -27,6 +30,10 @@ export default function Encrypt() {
     setThreeDESKey(event.target.value);
   }
 
+  function handleRSAPublicKey(event: ChangeEvent<HTMLInputElement>): void {
+    setRSAPublicKey(event.target.value);
+  }
+
   function handleSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
 
@@ -34,25 +41,76 @@ export default function Encrypt() {
 
     setText("");
     setThreeDESKey("");
+    setRSAPublicKey("");
+    setRSAPrivateKey("");
   }
 
   function handleGenerateThreeDESKey(event: MouseEvent<HTMLButtonElement>) {
     event.preventDefault();
 
-    const threeDESKey = CryptoJS.lib.WordArray.random(24).toString(
-      CryptoJS.enc.Hex
-    );
-    setThreeDESKey(threeDESKey);
+    setLoading(true);
+
+    setTimeout(() => {
+      const threeDESKey = CryptoJS.lib.WordArray.random(24).toString(
+        CryptoJS.enc.Hex
+      );
+
+      setThreeDESKey(threeDESKey);
+
+      setLoading(false);
+    }, 200);
   }
 
-  function handleRSAPublicKey(event: ChangeEvent<HTMLInputElement>): void {
-    throw new Error("Function not implemented.");
+  function handleGenerateRSAKeys(event: MouseEvent<HTMLButtonElement>) {
+    event.preventDefault();
+
+    setLoading(true);
+
+    setTimeout(() => {
+      const crypt = new JSEncrypt({ default_key_size: "2048" });
+
+      const RSAPublicKey = crypt.getPublicKey();
+      const RSAPrivateKey = crypt.getPrivateKey();
+
+      setRSAPublicKey(RSAPublicKey);
+      setRSAPrivateKey(RSAPrivateKey);
+
+      setLoading(false);
+    }, 200);
   }
 
   return (
     <div className="w-full max-w-xl">
       <div className="flex flex-col gap-6">
         <Card>
+          {loading && (
+            <p className="w-full flex items-center justify-center gap-2 bg-primary text-primary-foreground rounded-t-xl p-2">
+              <svg
+                className="text-primary-foreground/30 animate-spin w-4 h-4"
+                viewBox="0 0 64 64"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M32 3C35.8083 3 39.5794 3.75011 43.0978 5.20749C46.6163 6.66488 49.8132 8.80101 52.5061 11.4939C55.199 14.1868 57.3351 17.3837 58.7925 20.9022C60.2499 24.4206 61 28.1917 61 32C61 35.8083 60.2499 39.5794 58.7925 43.0978C57.3351 46.6163 55.199 49.8132 52.5061 52.5061C49.8132 55.199 46.6163 57.3351 43.0978 58.7925C39.5794 60.2499 35.8083 61 32 61C28.1917 61 24.4206 60.2499 20.9022 58.7925C17.3837 57.3351 14.1868 55.199 11.4939 52.5061C8.801 49.8132 6.66487 46.6163 5.20749 43.0978C3.7501 39.5794 3 35.8083 3 32C3 28.1917 3.75011 24.4206 5.2075 20.9022C6.66489 17.3837 8.80101 14.1868 11.4939 11.4939C14.1868 8.80099 17.3838 6.66487 20.9022 5.20749C24.4206 3.7501 28.1917 3 32 3L32 3Z"
+                  stroke="currentColor"
+                  strokeWidth="5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                ></path>
+                <path
+                  d="M32 3C36.5778 3 41.0906 4.08374 45.1692 6.16256C49.2477 8.24138 52.7762 11.2562 55.466 14.9605C58.1558 18.6647 59.9304 22.9531 60.6448 27.4748C61.3591 31.9965 60.9928 36.6232 59.5759 40.9762"
+                  stroke="currentColor"
+                  strokeWidth="5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="text-primary-foreground"
+                ></path>
+              </svg>
+              Carregando...
+            </p>
+          )}
+
           <CardHeader>
             <CardTitle className="text-2xl">Criptografar</CardTitle>
           </CardHeader>
@@ -105,39 +163,41 @@ export default function Encrypt() {
                     ></Input>
                     <Button
                       className="w-full sm:w-auto"
-                      onClick={handleGenerateThreeDESKey}
+                      onClick={handleGenerateRSAKeys}
                     >
                       <KeyRound />
                       Gerar
                     </Button>
                   </div>
                 </div>
-                {/* <div className="grid gap-2">
-                  <p className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
-                    Chave Privada RSA (2048 bits)
-                  </p>
-                  <p className="text-sm opacity-70 font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
-                    Salve-a em um lugar seguro e não a compartilhe, será útil
-                    para descriptografar a mensagem recebida
-                  </p>
-                  <div className="flex w-full rounded-md border border-input bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm">
-                    Sua chave privada RSA gerada aparecerá aqui...
+                {RSAPrivateKey && (
+                  <div className="grid gap-2">
+                    <p className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+                      Chave Privada RSA (2048 bits)
+                    </p>
+                    <p className="text-sm opacity-70 font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+                      Salve-a em um lugar seguro e não a compartilhe, será útil
+                      para descriptografar a mensagem recebida
+                    </p>
+                    <div className="flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm overflow-x-auto whitespace-nowrap scrollbar-hide">
+                      {RSAPrivateKey}
+                    </div>
                   </div>
-                </div> */}
+                )}
 
                 <Button type="submit">
                   <LockKeyhole />
                   Criptografar
                 </Button>
 
-                {/* <div className="grid gap-2">
+                <div className="grid gap-2">
                   <p className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
                     Dados a serem enviados
                   </p>
-                  <div className="flex w-full rounded-md border border-input bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm">
+                  <div className="flex w-full rounded-md border border-input bg-transparent px-3 py-1 text-base shadow-sm transition-colors break-all focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm text-wrap">
                     ...
                   </div>
-                </div> */}
+                </div>
               </div>
             </form>
           </CardContent>
